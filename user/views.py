e20c137904a40cbe0f38b29d41a67d121964c7cc
Sreelakshmi-c <csreelakshmi2@gmail.com>
2022-02-03 18:06:19 +0530

    homepage created

--- user/views.py
+++ user/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def fnHome(request):
     return render(request,'home.html')
+def fnFirst(request0):
+    return render(request0,'first.html')
