2790f15c1029d6b02d313858a9618bde17d6c9f8
Sreelakshmi-c <csreelakshmi2@gmail.com>
2022-02-24 19:17:46 +0530

    designs

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from .import views
 urlpatterns=[
-    path('',views.fnHome,name='home'),
-    path('first',views.fnFirst,name='first')
+    path('home',views.fnHome,name='home'),
+    path('first',views.fnFirst,name='first'),
+    path('contact',views.fnContact,name='contact'),
+    path('login',views.fnLogin,name='login'),
+    path('forgot',views.fnForgot,name='forgot'),
+    path('services',views.fnServices,name='services'),
+    path('reg',views.fnReg,name='reg')
+
+
 ]
--- user/views.py
+++ user/views.py
@@ -5,3 +5,15 @@ def fnHome(request):
     return render(request,'home.html')
 def fnFirst(request0):
     return render(request0,'first.html')
+def fnContact(request1):
+    return render(request1,'contact.html')
+def fnLogin(request2):
+    return render(request2,'login.html')
+def fnForgot(request3):
+    return render(request3,'forgot.html')
+def fnServices(request4):
+    return render(request4,'services.html')
+def fnReg(request5):
+    return render(request5,'reg.html')
+
+
